6fc66ec662b2f2579fb914ca6ca6829d67010daf
phum-athipgum <athiphumm@gmail.com>
2026-03-15 23:58:26 +0700

    feat : carousel , create carousel components and carousel story

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,16 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import tailwindcss from '@tailwindcss/vite';
 
 const config: StorybookConfig = {
   "stories": [
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": ["@storybook/addon-styling-webpack"],
-  "framework": "@storybook/react-vite"
+  "addons": [],
+  "framework": "@storybook/react-vite",
+  viteFinal: async (config) => {
+    config.plugins = config.plugins ?? [];
+    config.plugins.push(tailwindcss());
+    return config;
+  }
 };
 export default config;

7d789872874cd0c94e87c1d7e2e44b9091050e08
phum-athipgum <athiphumm@gmail.com>
2026-03-15 15:00:15 +0700

    chore : initial vite react story book

---
+++ .storybook/main.ts
@@ -0,0 +1,10 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": ["@storybook/addon-styling-webpack"],
+  "framework": "@storybook/react-vite"
+};
+export default config;
